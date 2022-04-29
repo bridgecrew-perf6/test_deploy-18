dce8afa205d36585f99a8d53178e6433abc11a04
m.lejwoda <m.lejwoda@sunshineprofits.com>
2022-04-29 18:35:11 +0200

    change url

--- test_deploy/urls.py
+++ test_deploy/urls.py
@@ -18,5 +18,5 @@ from django.urls import path,include
 
 urlpatterns = [
     path('admin/', admin.site.urls),
-    path('helloworld/', include('helloworld.urls'))
+    path('', include('helloworld.urls'))
 ]
